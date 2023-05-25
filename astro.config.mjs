0c8521beb62b8da9ceaa8e604b5863024be8877d
Jacky Zhao <j.zhao2k19@gmail.com>
2023-05-25 11:54:43 -0400

    fixed relative url processing, image support

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,17 @@ import tailwind from "@astrojs/tailwind"
 import { rehypeHeadingIds as slugifyHeaders } from '@astrojs/markdown-remark'
 import generateIdsForHeadings from 'rehype-slug'
 import linkHeadings from 'rehype-autolink-headings'
-import wikilinks from 'remark-wiki-link'
-import { slugFromPath, slugify, relativeToRoot, relative } from './src/util/path'
+import wikilinks from '@flowershow/remark-wiki-link'
+import { slugFromPath, slugify, relativeToRoot, relative, isAbsolute } from './src/util/path'
 import { visit } from 'unist-util-visit'
 import isAbsoluteUrl from 'is-absolute-url'
 import quartzConfig from './quartz.config.mjs'
 import remarkMath from 'remark-math'
 import rehypeKatex from 'rehype-katex'
+import { globbyStream } from 'globby'
+import fs from 'fs'
+import path from 'path'
+import { rimraf } from 'rimraf'
 
 const usesRelativePaths = quartzConfig.pathResolution === 'relative'
 const { enableLatex } = quartzConfig
@@ -24,14 +28,36 @@ export default defineConfig({
     preact({
       compat: true
     }),
-    tailwind()
+    tailwind(),
+    {
+      name: 'copy-images-to-public',
+      hooks: {
+        "astro:config:setup": async (options) => {
+          const assetsPath = path.join("public", "assets")
+          if (!options.isRestart) {
+            await rimraf(assetsPath)
+          }
+
+          // glob all non MD/MDX/HTML files in content folder and copy it over
+          for await (const fp of globbyStream("**", {
+            ignore: ["**/*.{md,mdx,html}"],
+            cwd: "./src/content/quartz",
+          })) {
+            const src = path.join("src", "content", "quartz", fp)
+            const dest = path.join(assetsPath, fp)
+            const dir = path.dirname(dest)
+            await fs.promises.mkdir(dir, { recursive: true }) // ensure dir exists
+            await fs.promises.copyFile(src, dest)
+          }
+        }
+      }
+    }
   ],
   markdown: {
     remarkPlugins: [
       [wikilinks, {
         pageResolver: (name) => ([slugify(name)]),
-        aliasDivider: '|',
-        hrefTemplate: x => x
+        pathFormat: usesRelativePaths ? 'raw' : 'obsidian-absolute'
       }],
       ...enableLatex ? [remarkMath] : [],
     ],
@@ -39,25 +65,43 @@ export default defineConfig({
       // relative link processing
       () => (tree, vfile) => {
         const curSlug = slugFromPath(vfile.cwd, vfile.history[0])
+        const transformLink = (target) => {
+          if (!isAbsoluteUrl(target)) {
+            const targetSlug = slugify(decodeURI(target))
+            if (usesRelativePaths && !isAbsolute(targetSlug)) {
+              return './' + relative(curSlug, targetSlug)
+            } else {
+              return './' + relativeToRoot(curSlug, targetSlug)
+            }
+          }
+        }
+
+        // rewrite all links
         visit(tree, 'element', (node, _index, _parent) => {
           if (
             node.tagName === 'a' &&
             node.properties &&
             typeof node.properties.href === 'string'
           ) {
-            if (!isAbsoluteUrl(node.properties.href)) {
-              const targetSlug = slugify(decodeURI(node.properties.href))
-              if (usesRelativePaths) {
-                node.properties.href = relativeToRoot(curSlug, targetSlug)
-              } else {
-                node.properties.href = relative(curSlug, targetSlug)
-              }
-            }
+            node.properties.href = transformLink(node.properties.href)
+          }
+        })
+        
+        // transform all images
+        visit(tree, 'element', (node, _index, _parent) => {
+          if (
+            node.tagName === 'img' &&
+            node.properties &&
+            typeof node.properties.src === 'string'
+          ) {
+            const ext = path.extname(node.properties.src)
+            node.properties.src = transformLink("/assets/" + node.properties.src) + ext
           }
         })
       },
       slugifyHeaders,
       generateIdsForHeadings,
+      // TODO: fix this
       [linkHeadings, { content: '#' }],
       ...enableLatex ? [[rehypeKatex, { output: 'html' }]] : []
     ],
--- src/util/path.ts
+++ src/util/path.ts
@@ -2,21 +2,20 @@ import path from 'path'
 import { slug } from 'github-slugger'
 
 export function resolveToRoot(slug: string): string {
-  let fp = slug
+  let fp = slug === '.' ? '/' : slug
   if (fp.endsWith("/index")) {
     fp = fp.slice(0, -"/index".length)
   }
 
   const newPath = fp
     .split(path.posix.sep)
+    .filter(x => x !== '')
     .map(_ => '..')
     .join(path.posix.sep)
+
   return "./" + newPath 
 }
 
-export function relativeToRoot(slug: string, fp: string): string {
-  return path.join(resolveToRoot(slug), fp)
-}
 
 // mostly from https://github.com/withastro/astro/blob/dc31b8a722136eff90a600380a6419a37808d614/packages/astro/src/content/utils.ts#L213
 export function slugify(fp: string): string {
@@ -25,14 +24,24 @@ export function slugify(fp: string): string {
   const slugParts: string = rawSlugSegments
     .map((segment) => slug(segment))
     .join(path.posix.sep)
-    .replace(/\/index$/, '')
+    .replace(/index$/, '')
+    .replace(/\/$/, '')
   return path.normalize(slugParts)
 }
 
 export function slugFromPath(dir: string, fullPath: string): string {
-  return slugify(path.relative(dir, fullPath))
+  const fullDir = path.join(dir, "src", "content", "quartz")
+  return slugify(path.relative(fullDir, fullPath))
+}
+
+export function relativeToRoot(slug: string, fp: string): string {
+  return path.join(resolveToRoot(slug), fp)
 }
 
 export function relative(src: string, dest: string): string {
   return path.relative(src, dest)
+}
+
+export function isAbsolute(fp: string): boolean {
+  return path.isAbsolute(fp)
 }
